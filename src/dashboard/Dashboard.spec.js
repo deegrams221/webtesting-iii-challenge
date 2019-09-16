3bb39e5e503809846c8c626440b07cdcfada1c81
Diana Grams <deegrams221@gmail.com>
2019-09-16 14:43:51 -0600

    dashboard tests setup

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  // snapshot test
+  it('should match snapshot', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+  });
+
+  // create test cases
+  it('should render controls & display', () => {
+    const {getByText} = render(<Dashboard />);
+      getByText(/unlocked/i);
+      getByText(/open/i);
+      getByText(/lock gate/i);
+      getByText(/close gate/i);
+  });
+});
